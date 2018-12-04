ff7c3d9d42e4036ce4da7452a9d370a3b930d2fb
karilint <kari.lintulaakso@gmail.com>
2018-12-04 16:57:36 +0200

    Added name_detail.html.

--- rnames_app/urls.py
+++ rnames_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.name_list, name='name_list'),
+    path('name/<int:pk>/', views.name_detail, name='name_detail'),
 ]
--- rnames_app/views.py
+++ rnames_app/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Name
 
 def name_list(request):
     names = Name.objects.order_by('name')
     return render(request, 'rnames_app/name_list.html', {'names': names})
+
+def name_detail(request, pk):
+    name = get_object_or_404(Name, pk=pk)
+    return render(request, 'rnames_app/name_detail.html', {'name': name})
