60c5d6ff0e4600a079d98ece88bc1c92a6c65b6b
karilint <kari.lintulaakso@gmail.com>
2018-12-05 11:55:26 +0200

    Added views to create/edit Names

--- rnames_app/urls.py
+++ rnames_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.name_list, name='name_list'),
     path('name/<int:pk>/', views.name_detail, name='name_detail'),
+    path('name/new', views.name_new, name='name_new'),
+    path('name/<int:pk>/edit/', views.name_edit, name='name_edit'),
 ]
--- rnames_app/views.py
+++ rnames_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Name
+from .forms import NameForm
 
 def name_list(request):
     names = Name.objects.order_by('name')
@@ -8,3 +10,30 @@ def name_list(request):
 def name_detail(request, pk):
     name = get_object_or_404(Name, pk=pk)
     return render(request, 'rnames_app/name_detail.html', {'name': name})
+
+def name_new(request):
+    if request.method == "POST":
+        form = NameForm(request.POST)
+        if form.is_valid():
+            name = form.save(commit=False)
+            name.created_by_id = request.user.id
+            name.created_at = timezone.now()
+            name.save()
+            return redirect('name_detail', pk=name.pk)
+    else:
+        form = NameForm()
+    return render(request, 'rnames_app/name_edit.html', {'form': form})
+
+def name_edit(request, pk):
+    name = get_object_or_404(Name, pk=pk)
+    if request.method == "POST":
+        form = NameForm(request.POST, instance=name)
+        if form.is_valid():
+            name = form.save(commit=False)
+            name.created_by_id = request.user.id
+            name.created_at = timezone.now()
+            name.save()
+            return redirect('name_detail', pk=name.pk)
+    else:
+        form = NameForm(instance=name)
+    return render(request, 'rnames_app/name_edit.html', {'form': form})
