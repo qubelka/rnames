69e3f084c365bea8d35111c4da99a311b8d8a8fa
Jonne Sotala <jonne.sotala@gmail.com>
2021-09-01 16:21:33 +0300

    Project cleanup & added settings.py file

--- mb/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MbConfig(AppConfig):
-    name = 'mb'
--- rnames_app/admin.py
+++ rnames_app/admin.py
@@ -1,23 +1,16 @@
 from django.contrib import admin
 from simple_history.admin import SimpleHistoryAdmin
-from rnames_app.models import(Binning
-    , Name
-    , Relation
-    , Reference
-    , Location
-    , QualifierName
-    , StratigraphicQualifier
-    , Qualifier
-    , StructuredName)
-from mb import models
+from rnames_app.models import(Binning, Name, Relation, Reference, Location,
+                              QualifierName, StratigraphicQualifier, Qualifier, StructuredName)
 
 # admin.site.register([Name, Reference, Location, QualifierName, StratigraphicQualifier, Qualifier, StructuredName, Relation,])
 
-#@admin.register(Name)
-#class NameAdmin(admin.ModelAdmin):
+# @admin.register(Name)
+# class NameAdmin(admin.ModelAdmin):
 #    list_display = ['name', ]
 #    search_fields = ['name', ]
 #    history_list_display = ['name', ]
+
 
 @admin.register(Reference)
 class ReferenceAdmin(admin.ModelAdmin):
@@ -25,19 +18,23 @@ class ReferenceAdmin(admin.ModelAdmin):
     list_display_links = ['first_author', ]
     search_fields = ['first_author', 'title', ]
 
+
 class RelationAdmin(admin.ModelAdmin):
     raw_id_fields = ('name_one', 'name_two',)
 #    search_fields = ['structuredname__name']
 #    search_fields = ['foreign_key__related_fieldname']
+
 
 class RelationHistoryAdmin(SimpleHistoryAdmin):
     history_list_display = ['belongs_to', ]
     raw_id_fields = ('name_one', 'name_two',)
     search_fields = ['name_one__name__name', 'name_two__name__name', ]
 
+
 class StructuredNameHistoryAdmin(SimpleHistoryAdmin):
     history_list_display = ['qualifier', 'name', 'location', ]
     search_fields = ['name__name', ]
+
 
 admin.site.register(Binning, SimpleHistoryAdmin)
 admin.site.register(Name, SimpleHistoryAdmin)
@@ -49,14 +46,3 @@ admin.site.register(Qualifier, SimpleHistoryAdmin)
 #admin.site.register(StructuredName, SimpleHistoryAdmin)
 admin.site.register(Relation, RelationHistoryAdmin)
 admin.site.register(StructuredName, StructuredNameHistoryAdmin)
-admin.site.register(models.ChoiceValue, SimpleHistoryAdmin)
-admin.site.register(models.DietSet, SimpleHistoryAdmin)
-admin.site.register(models.DietSetItem, SimpleHistoryAdmin)
-admin.site.register(models.EntityClass, SimpleHistoryAdmin)
-admin.site.register(models.EntityRelation, SimpleHistoryAdmin)
-admin.site.register(models.FoodItem, SimpleHistoryAdmin)
-admin.site.register(models.MasterEntity, SimpleHistoryAdmin)
-admin.site.register(models.MasterReference, SimpleHistoryAdmin)
-admin.site.register(models.RelationClass, SimpleHistoryAdmin)
-admin.site.register(models.SourceEntity, SimpleHistoryAdmin)
-admin.site.register(models.SourceReference, SimpleHistoryAdmin)
