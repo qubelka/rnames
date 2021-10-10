a4a1151533dda3a0e2b6e35c4a402e3d30c62ad6
chzesa <chzesa@gmail.com>
2021-10-10 18:36:44 +0300

    Iterate directly over names

--- app/rnames_app/utils/binning_fun.py
+++ app/rnames_app/utils/binning_fun.py
@@ -314,22 +314,20 @@ def rule2(results, c_rels_d, t_scheme, runrange, used_ts, xnames_raw, b_scheme):
     return resi_2
 
 def bin_unique_names_1(ibs, x1, used_ts, xnames_raw):
-    bnu = x1["name_1"]
-    bnu = bnu.drop_duplicates()
-    bnurange = np.arange(0,len(bnu),1)
+    bnu = x1["name_1"].unique()
 
     x3 = pd.DataFrame([] * 5, index=["name", "oldest", "youngest", "ts_count", "refs"])
     x3 = pd.DataFrame.transpose(x3)
     x3a_frames = [x3]
-    for i in bnurange:
+    for name in bnu:
         if ibs == 0:
-            x3a = bifu_s2(x1.loc[x1["name_1"]==bnu.iloc[i]], used_ts, xnames_raw)
+            x3a = bifu_s2(x1.loc[x1["name_1"]==name], used_ts, xnames_raw)
             x3a_frames.append(x3a)
         if ibs == 1:
-            x3a = bifu_y2(x1.loc[x1["name_1"]==bnu.iloc[i]], used_ts, xnames_raw)
+            x3a = bifu_y2(x1.loc[x1["name_1"]==name], used_ts, xnames_raw)
             x3a_frames.append(x3a)
         if ibs == 2:
-            x3a = bifu_c2(x1.loc[x1["name_1"]==bnu.iloc[i]], used_ts, xnames_raw)
+            x3a = bifu_c2(x1.loc[x1["name_1"]==name], used_ts, xnames_raw)
             x3a_frames.append(x3a)
 
     x3 = pd.concat(x3a_frames, axis=0, sort=True)
