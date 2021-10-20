95effc0308b4f2f45dc7de30b917ad2fbbf45397
chzesa <chzesa@gmail.com>
2021-10-21 00:32:47 +0300

    Create db table for binning time slices

--- app/rnames_app/models.py
+++ app/rnames_app/models.py
@@ -374,3 +374,16 @@ class Relation(BaseModel):
         String for representing the Model object (in Admin site etc.)
         """
         return '%s | %s' % (self.name_one, self.name_two)
+
+class TimeSlice(BaseModel):
+    name = models.CharField(max_length=200, blank=False)
+    order = models.IntegerField(help_text='Chronological order within scheme.')
+    scheme = models.CharField(max_length=200, blank=False)
+
+    class Meta:
+        ordering = ['scheme', 'name']
+        unique_together= [['scheme', 'order']]
+
+    def __str__(self):
+        return '%s %d %s' % (self.scheme, self.order, self.name)
+
