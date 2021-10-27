caf4c70fa7658e4972959f18c7f9134fed9b9aa6
Qubelka <42945433+qubelka@users.noreply.github.com>
2021-10-27 07:36:30 -0700

    Create separate reducer for names

--- app/frontend/src/store/map/actions.js
+++ app/frontend/src/store/map/actions.js
@@ -1,9 +1,4 @@
-import {
-	MAP_VALUE,
-	INITIALIZE_MAP_VALUES,
-	ADD_NAME,
-	UPDATE_NAME,
-} from '../action-types'
+import { MAP_VALUE, INITIALIZE_MAP_VALUES } from '../action-types'
 
 export const mapId = (key, value) => {
 	return {
@@ -17,22 +12,5 @@ export const initMapvalues = map => {
 	return {
 		type: INITIALIZE_MAP_VALUES,
 		map,
-	}
-}
-
-export const addName = (refId, name) => {
-	return {
-		type: ADD_NAME,
-		refId,
-		name,
-	}
-}
-
-export const updateName = (refId, name, nameId) => {
-	return {
-		type: UPDATE_NAME,
-		refId,
-		name,
-		nameId,
 	}
 }
