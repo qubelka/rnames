df7b3784160389954a70f32b6916455f05483c06
tkoukkar <32363795+tkoukkar@users.noreply.github.com>
2021-11-03 22:59:28 +0200

    Update test_views.py

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 class IndexViewTest(TestCase):
     def test_view_url_exists_at_desired_location(self):
         response = self.client.get('/index/')
-        self.assertEqual(response.status_code, 418)
+        self.assertEqual(response.status_code, 200)
 
     def test_view_uses_correct_template(self):
         response = self.client.get(reverse('index'))
