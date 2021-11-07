e70a8e5a3e3cdf7aed37f74feaadcb890f5542b0
Qubelka <42945433+qubelka@users.noreply.github.com>
2021-11-07 04:08:48 -0800

    Add validations for ref and submission

--- app/frontend/src/components/Submit.js
+++ app/frontend/src/components/Submit.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import axios from 'axios'
+import { replaceEmptyStringsWithNullInReference } from '../validations'
 
 export const Submit = () => {
 	const data = useSelector(v => {
@@ -13,15 +14,33 @@ export const Submit = () => {
 	})
 
 	const submit = async () => {
+		if (data.reference && data.reference.edit) {
+			console.log(
+				'Please save changes made in reference before submitting'
+			)
+			return
+		}
+
+		if (data.structured_names.length === 0 || data.relations.length === 0) {
+			console.log(
+				'Please add structured names and relations before submitting'
+			)
+			return
+		}
+
 		const parseIds = (obj, keys = []) => {
 			const ret = { ...obj }
 			keys.forEach(k => (ret[k] = JSON.parse(ret[k])))
 			return ret
 		}
 
+		const refWithNullValues = replaceEmptyStringsWithNullInReference(
+			data.reference
+		)
+
 		const submit_data = {
 			names: data.names.map(v => parseIds(v, ['id'])),
-			reference: parseIds(data.reference, ['id']),
+			reference: parseIds(refWithNullValues, ['id']),
 			relations: data.relations.map(v =>
 				parseIds(v, ['id', 'name1', 'name2'])
 			),
@@ -51,7 +70,7 @@ export const Submit = () => {
 			data: submit_data,
 		})
 			// Django cleans fields when filling templates so this is safe
-			.then(res => document.body.innerHTML = res.data)
+			.then(res => (document.body.innerHTML = res.data))
 			.catch(err => console.log(err))
 	}
 
---
+++ app/frontend/src/validations.js
@@ -0,0 +1,112 @@
+const urlIsValid = url => {
+	let parsedUrl
+	try {
+		parsedUrl = new URL(url)
+	} catch (err) {
+		return false
+	}
+
+	return ['https:', 'http:'].includes(parsedUrl.protocol)
+}
+
+export const referenceFormIsValid = (firstAuthor, year, title, doi, link) => {
+	let valid = true
+
+	if (firstAuthor !== '') {
+		if (firstAuthor.length > 50) {
+			console.log(
+				`Ensure the author name is at most 50 characters (name has ${firstAuthor.length} characters)`
+			)
+			valid = false
+		}
+	}
+
+	if (year !== '') {
+		yearValidation: {
+			if (year) {
+				if (year === '0') {
+					console.log('Please provide correct year value')
+					valid = false
+					break yearValidation
+				}
+
+				const yearAsNumber = Number(year)
+				if (!yearAsNumber) {
+					console.log('Year must be number')
+					valid = false
+					break yearValidation
+				}
+
+				if (yearAsNumber < 1800 || yearAsNumber > 2100) {
+					console.log('Please provide correct year value')
+					valid = false
+				}
+			}
+		}
+	}
+
+	if (title === '') {
+		console.log('Please provide the title of the reference')
+		valid = false
+	} else {
+		if (title.length > 250) {
+			console.log(
+				`Ensure the title is at most 250 characters (title has ${title.length} characters)`
+			)
+			valid = false
+		}
+	}
+
+	if (doi !== '') {
+		if (doi.substring(0, 3) !== '10.') {
+			console.log(
+				'Enter the DOI number that begins with 10 followed by a period'
+			)
+			valid = false
+		}
+	}
+
+	if (link !== '') {
+		urlValidation: {
+			if (!urlIsValid(link)) {
+				console.log('Please enter correct url')
+				valid = false
+				break urlValidation
+			}
+			if (link.length > 200) {
+				console.log(
+					`Ensure the url is at most 200 characters (url has ${link.length} characters)`
+				)
+				valid = false
+			}
+		}
+	}
+
+	return valid
+}
+
+/* All fields except the title can be null.*/
+export const replaceEmptyStringsWithNullInReference = reference => {
+	const updatedReference = { ...reference }
+	Object.keys(updatedReference).forEach(k => {
+		if (updatedReference[k] === '') {
+			updatedReference[k] = null
+		}
+	})
+	return updatedReference
+}
+
+export const doiFormIsValid = doi => {
+	if (doi === '') {
+		console.log('Please provide the doi number')
+		return false
+	} else {
+		if (doi.substring(0, 3) !== '10.') {
+			console.log(
+				'Enter the DOI number that begins with 10 followed by a period'
+			)
+			return false
+		}
+	}
+	return true
+}
