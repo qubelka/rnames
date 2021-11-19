1eb00ab04c720af86491f3ea7d24ee322fcdb8bc
Qubelka <42945433+qubelka@users.noreply.github.com>
2021-11-19 02:38:49 -0800

    Add doi tests that do not require function mocking

--- app/frontend/src/components/__tests__/DoiForm.test.js
+++ app/frontend/src/components/__tests__/DoiForm.test.js
@@ -1,5 +1,67 @@
-import { expect, test } from '@jest/globals'
+import React from 'react'
+import { expect, test, beforeEach, describe } from '@jest/globals'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { DoiForm } from '../DoiForm'
 
-test('pass', () => {
-	expect(true).toBe(true)
+const doi = '10.0'
+const setQueried = () => {}
+const setFirstAuthor = () => {}
+const setYear = () => {}
+const setTitle = () => {}
+const setDoi = () => {}
+const setLink = () => {}
+
+describe('When reference form set to visible', () => {
+	const displayRefForm = 'block'
+
+	beforeEach(() => {
+		render(
+			<DoiForm
+				{...{
+					doi,
+					setQueried,
+					setFirstAuthor,
+					setYear,
+					setTitle,
+					setDoi,
+					setLink,
+					displayRefForm,
+				}}
+			/>
+		)
+	})
+
+	test('rendered doi form has two buttons', () => {
+		expect(screen.getAllByRole('button')).toHaveLength(2)
+	})
+
+	test('rendered doi form has input field', () => {
+		expect(screen.getByRole('textbox')).toBeInTheDocument()
+	})
+})
+
+describe('When reference form set to invisible', () => {
+	const displayRefForm = 'none'
+
+	beforeEach(() => {
+		render(
+			<DoiForm
+				{...{
+					doi,
+					setQueried,
+					setFirstAuthor,
+					setYear,
+					setTitle,
+					setDoi,
+					setLink,
+					displayRefForm,
+				}}
+			/>
+		)
+	})
+
+	test('doi input field not visible', () => {
+		expect(screen.queryByRole('textbox')).toBeNull()
+	})
 })
