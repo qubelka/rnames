812a8e90379f3b2d2d4884af68fceb56cccd08e9
chzesa <chzesa@gmail.com>
2021-12-05 11:20:59 +0200

    Create shortest binning functions which do not rely on sorting order

--- app/rnames_app/utils/rn_funs.py
+++ app/rnames_app/utils/rn_funs.py
@@ -96,3 +96,57 @@ def bifu_s2(col, ntts):
     max_y = max(bio_setb[:, col.ntts.reference_year])
     # ntts is sorted by reference year so binary search can be used
     return bio_setb[bisect_left(bio_setb[:, col.ntts.reference_year], max_y):bisect_right(bio_setb[:, col.ntts.reference_year], max_y)]
+
+def bifu_s3(col, ntts):
+    rows = []
+
+    for ref in pd.unique(ntts[:, col.ntts.reference_id]):
+        cptx = ntts[ntts[:, col.ntts.reference_id]== ref]
+        ts_min = np.min(cptx[:, col.ntts.ts_index])
+        ts_max = np.max(cptx[:, col.ntts.ts_index])
+        ts_x = ts_max - ts_min
+        rows.append((ref, ts_x))
+    rows = np.array(rows)
+
+    # Select references with minimal time slice delta
+    min_ts = np.min(rows[:, 1])
+    short_ref = rows[rows[:, 1] == min_ts]
+
+    # Select rows whose reference has minimal time slice delta
+    bio_setb = ntts[np.isin(ntts[:, col.ntts.reference_id], short_ref[:, 0])]
+
+    # search for youngest reference among those
+    max_y = np.max(bio_setb[:, col.ntts.reference_year])
+    return bio_setb[bio_setb[:, col.ntts.reference_year] == max_y]
+
+def bifu_s4(col, ntts):
+    # select all references
+    rows = []
+    min_delta = np.inf
+
+    # search for shortest range
+    # Append all reference ids matching the shortest found range (min_delta) to rows
+    # If shorter range is found, clear rows and lower min_delta to the new range
+    sorted_refs = ntts[ntts[:, col.ntts.reference_id].argsort()]
+    for r_yx in np.unique(ntts[:, col.ntts.reference_id]):
+        # sorted_refs is sorted by reference id so finding all entries matching the refs can be done quickly with binary search
+        # Select rows with this reference
+        cptx_begin = bisect_left(sorted_refs[:, col.ntts.reference_id], r_yx)
+        cptx_end = bisect_right(sorted_refs[:, col.ntts.reference_id], r_yx)
+        cptx = sorted_refs[cptx_begin:cptx_end]
+
+        # Calculate range delta
+        ts_x = np.max(cptx[:, col.ntts.youngest_index]) - np.min(cptx[:, col.ntts.oldest_index])
+
+        if ts_x == min_delta:
+            rows.append(r_yx)
+        if ts_x < min_delta:
+            del rows[:]
+            min_delta = ts_x
+            rows.append(r_yx)
+
+    bio_setb = ntts[np.isin(ntts[:, col.ntts.reference_id], rows)]
+    # search for youngest reference among those
+    max_y = max(bio_setb[:, col.ntts.reference_year])
+    # ntts is sorted by reference year so binary search can be used
+    return bio_setb[bio_setb[:, col.ntts.reference_year] == max_y]
