2b01cb59562888d707d9fc8900b8a36ecaefd458
Qubelka <42945433+qubelka@users.noreply.github.com>
2021-12-08 23:14:39 -0800

    Update Relation tests after pr 75

--- app/frontend/src/components/__tests__/Relation.test.js
+++ app/frontend/src/components/__tests__/Relation.test.js
@@ -7,17 +7,83 @@ import configureStore from 'redux-mock-store'
 import { Provider } from 'react-redux'
 import { Relation } from '../Relation'
 import { relReducer } from '../../store/relations/reducers'
+import { mapReducer } from '../../store/map/reducers'
+import { makeId } from '../../utilities'
 
 const mockStore = configureStore([])
 
+const dbStructuredName1 = {
+	id: '{"type":"db_structured_name","value":1296}',
+	name_id: '{"type":"db_name","value":1126}',
+	location_id: '{"type":"db_location","value":70}',
+	qualifier_id: '{"type":"db_qualifier","value":7}',
+	reference_id: '{"type":"db_reference","value":0}',
+	remarks: null,
+}
+
+const dbStructuredName2 = {
+	id: '{"type":"db_structured_name","value":5031}',
+	location_id: '{"type":"db_location","value":1}',
+	name_id: '{"type":"db_name","value":4644}',
+	qualifier_id: '{"type":"db_qualifier","value":5}',
+	reference_id: '{"type":"db_reference","value":0}',
+	remarks: null,
+}
+
 const relation = {
-	id: '{"type":"relation","value":3}',
-	name1: '{"type":"db_structured_name","value":4422}',
-	name2: '{"type":"structured_name","value":2}',
+	id: makeId('relation'),
+	name1: [dbStructuredName1.id],
+	name2: [dbStructuredName2.id],
 	belongs_to: 0,
 	reference_id: -1,
-	formattedName1: 'Basisletta / Member / Norway',
-	formattedName2: 'Abercwmeiddaw / Formation / Saskatchewan',
+}
+
+const formattedName1 = 'Yeoman / Formation / Saskatchewan'
+const formattedName2 = 'Aalenian / Stage / Global'
+
+const initialMapState = {
+	[dbStructuredName1.id]: {
+		...dbStructuredName1,
+	},
+	[dbStructuredName1.name_id]: {
+		id: [dbStructuredName1.name_id],
+		name: 'Yeoman',
+	},
+	[dbStructuredName1.location_id]: {
+		id: [dbStructuredName1.location_id],
+		name: 'Saskatchewan',
+	},
+	[dbStructuredName1.qualifier_id]: {
+		id: [dbStructuredName1.qualifier_id],
+		level: 2,
+		qualifier_name_id: '{"type":"db_qualifier_name","value":7}',
+		stratigraphic_qualifier_id: 2,
+	},
+	'{"type":"db_qualifier_name","value":7}': {
+		id: '{"type":"db_qualifier_name","value":7}',
+		name: 'Formation',
+	},
+	[dbStructuredName2.id]: {
+		...dbStructuredName2,
+	},
+	[dbStructuredName2.name_id]: {
+		id: [dbStructuredName2.name_id],
+		name: 'Aalenian',
+	},
+	[dbStructuredName2.location_id]: {
+		id: [dbStructuredName2.location_id],
+		name: 'Global',
+	},
+	[dbStructuredName2.qualifier_id]: {
+		id: [dbStructuredName2.qualifier_id],
+		level: 5,
+		qualifier_name_id: '{"type":"db_qualifier_name","value":5}',
+		stratigraphic_qualifier_id: 1,
+	},
+	'{"type":"db_qualifier_name","value":5}': {
+		id: '{"type":"db_qualifier_name","value":5}',
+		name: 'Stage',
+	},
 }
 
 describe('When relation without inclusion rendered', () => {
@@ -25,17 +91,14 @@ describe('When relation without inclusion rendered', () => {
 	beforeEach(() => {
 		store = mockStore({
 			rel: relReducer([], { type: 'INIT', rel: {} }),
+			map: mapReducer(initialMapState, { type: 'INIT' }),
 		})
 		store.dispatch = jest.fn()
 		render(
 			<Provider store={store}>
 				<table>
 					<tbody>
-						<Relation
-							relation={relation}
-							formattedName1={relation.formattedName1}
-							formattedName2={relation.formattedName2}
-						/>
+						<Relation relation={relation} />
 					</tbody>
 				</table>
 			</Provider>
@@ -49,7 +112,7 @@ describe('When relation without inclusion rendered', () => {
 
 	test('shows structured names forming a relation on the same row', () => {
 		const relationRow = screen.getByRole('row', {
-			name: `${relation.formattedName1} ↔ ${relation.formattedName2}`,
+			name: `${formattedName1} ↔ ${formattedName2}`,
 		})
 		expect(relationRow).toBeInTheDocument()
 	})
@@ -93,17 +156,14 @@ describe('When relation with inclusion rendered', () => {
 	beforeEach(() => {
 		store = mockStore({
 			rel: relReducer([], { type: 'INIT', rel: {} }),
+			map: mapReducer(initialMapState, { type: 'INIT' }),
 		})
 		store.dispatch = jest.fn()
 		render(
 			<Provider store={store}>
 				<table>
 					<tbody>
-						<Relation
-							relation={relationWithInclusion}
-							formattedName1={relation.formattedName1}
-							formattedName2={relation.formattedName2}
-						/>
+						<Relation relation={relationWithInclusion} />
 					</tbody>
 				</table>
 			</Provider>
