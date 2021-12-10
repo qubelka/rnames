353db6ce996438b2a9d45403f64b5345841bc738
chzesa <chzesa@gmail.com>
2021-12-10 16:10:08 +0200

    Style Notification

--- app/frontend/src/components/Notification.js
+++ app/frontend/src/components/Notification.js
@@ -2,13 +2,24 @@ import React from 'react'
 import './notification.css'
 
 export const Notification = ({ notification }) => {
-  if (notification === null) {
-    return null
-  }
+    if (notification === null) {
+        return null
+    }
 
-  return (
-    <div className={notification.type}>
-      {notification.message}
-    </div>
-  )
+    let className
+
+    switch (notification.type) {
+        case 'error':
+            className = 'w3-panel w3-border w3-red w3-border-dark-gray'
+            break
+        case 'information':
+            className = 'w3-panel w3-border w3-blue w3-border-dark-gray'
+            break
+    }
+
+    return (
+        <div className={className}>
+            <p>{notification.message}</p>
+        </div>
+    )
 }
