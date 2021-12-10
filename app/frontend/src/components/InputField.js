2b53bdf7c56fe2e79880c8d94a99bb3e96c749c4
chzesa <chzesa@gmail.com>
2021-12-10 17:23:29 +0200

    Make horizontal variant of input field

--- app/frontend/src/components/InputField.js
+++ app/frontend/src/components/InputField.js
@@ -1,6 +1,38 @@
 import React from 'react'
 import { Notification } from './Notification'
 
+export const HorizontalInputField = ({
+	name,
+	value,
+	notification,
+	setField,
+	autoFocus,
+}) => {
+	return (
+		<div className='w3-row'>
+			<div className='w3-quarter'>
+				<label htmlFor={name}>
+					<p>
+						<b>{name}</b>
+					</p>
+				</label>
+			</div>
+			<div className='w3-rest'>
+				<input
+					className='w3-input w3-border w3-border-dark-grey w3-rest'
+					type='text'
+					name={name}
+					value={value}
+					onChange={e => setField(e.target.value)}
+					autoFocus={autoFocus}
+				/>
+			</div>
+
+			<Notification notification={notification} />
+		</div>
+	)
+}
+
 export const InputField = ({
 	name,
 	value,
